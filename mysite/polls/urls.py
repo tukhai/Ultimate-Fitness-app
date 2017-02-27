95510ad37ccc73955c974041f642d50b1310881e
NguyenTuKhai <ntukhai@gmail.com>
2017-02-27 18:41:36 +0800

    From the Starting Tutorial Guide of Django, adding the Organic Template to the polls/templates/polls => move the non-html folders to poll/static/polls => change the directory in index.html, contact.html so can run the static files on html => add contact.html, about-us.html urls to url.py

---
+++ mysite/polls/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    # ex: /polls/
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    # ex: /polls/5/
+    url(r'^contact.html/$', views.ContactView.as_view(), name='contact'),
+    # ex: /polls/5/
+    url(r'^about-us.html/$', views.AboutView.as_view(), name='about'),
+    # ex: /polls/5/
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+]
