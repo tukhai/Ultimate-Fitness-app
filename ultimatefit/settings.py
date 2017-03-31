fb9cf9bb531ca816e78708efc5d58a2e2a0ea60d
TheAnh <theanh0512@gmail.com>
2017-03-31 14:08:33 +0800

    change email setting

--- ultimatefit/settings.py
+++ ultimatefit/settings.py
@@ -135,9 +135,9 @@ LOGIN_REDIRECT_URL = '/shop.html/'
 
 # Email settings
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "twohourbinhyen@gmail.com"
-EMAIL_HOST_PASSWORD = "iahk9425"
-EMAIL_PORT = 587
+EMAIL_HOST = os.environ.get("EMAIL_HOST", '')
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", '')
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", '')
+EMAIL_PORT = os.environ.get("EMAIL_PORT", '')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "twohourbinhyen@gmail.com"
