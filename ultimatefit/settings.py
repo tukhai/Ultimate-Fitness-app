09206102d09aac3eb06b04f703782d2fca6d6167
DESKTOP-MOSITAJ\ANH00 <theanh0512@gmail.com>
2018-02-28 04:06:38 +0800

    change setting

--- ultimatefit/settings.py
+++ ultimatefit/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-#STATIC_ROOT = 'D:\home\site\wwwroot\static'
+STATIC_ROOT = 'D:\home\site\wwwroot\static'
 STATIC_URL = '/static/'
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 STATICFILES_DIRS = (
