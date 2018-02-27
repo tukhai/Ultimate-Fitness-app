722702ec04fe39bf236636e751860bb213303a0e
DESKTOP-MOSITAJ\ANH00 <theanh0512@gmail.com>
2018-02-28 04:03:57 +0800

    change setting

--- ultimatefit/settings.py
+++ ultimatefit/settings.py
@@ -139,8 +139,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_ROOT = 'D:\home\site\wwwroot\static'
+#STATIC_ROOT = 'D:\home\site\wwwroot\static'
 STATIC_URL = '/static/'
+SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
+STATICFILES_DIRS = (
+  os.path.join(SITE_ROOT, 'static/'),
+)
 
 # Registration
 ACCOUNT_ACTIVATION_DAYS = 7
