17eea22b0f6d9db74781c41024f2e8e78665d63a
DESKTOP-MOSITAJ\ANH00 <theanh0512@gmail.com>
2018-02-28 03:20:13 +0800

    change debug to false

--- ultimatefit/settings.py
+++ ultimatefit/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^aypaf)x48l)2im7&fa+n&3f0v8_lqdb4padz^@9f0x-7hz*#!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ultimatefitapp.azurewebsites.net', 'localhost']
 
