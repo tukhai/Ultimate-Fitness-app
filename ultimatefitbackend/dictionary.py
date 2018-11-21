2d83b8d977f3e775c938742a383e6e76be8750af
tukhai <ntukhai@gmail.com>
2018-11-22 00:56:54 +0800

    change dict structure, throw error if type wrong translate key

--- ultimatefitbackend/dictionary.py
+++ ultimatefitbackend/dictionary.py
@@ -3,11 +3,11 @@ import json
 
 def dictionary_translation():
 	dict_trans = {
-		'gallery': {
+		'lbl_gallery': {
 			'vn': 'Ảnh',
 			'en': 'gallery'
 		},
-		'ADD TO CART': {
+		'lbl_ADD_TO_CART': {
 			'vn': 'THÊM VÀO GIỎ',
 			'en': 'ADD TO CART'
 		},
@@ -15,57 +15,61 @@ def dictionary_translation():
 			'vn': 'dưa hấu',
 			'en': 'watermelon'
 		},
-		'Home': {
+		'lbl_Home': {
 			'vn': 'Trang Chủ',
 			'en': 'Home'
 		},
-		'About us': {
+		'lbl_About_us': {
 			'vn': 'Giới thiệu',
 			'en': 'About us'
 		},
-		'Shop': {
+		'lbl_Shop': {
 			'vn': 'cửa hàng',
 			'en': 'Shop'
 		},
-		'Shop By Calendar': {
+		'lbl_Shop_By_Calendar': {
 			'vn': 'Lịch Đặt Hàng',
 			'en': 'Shop By Calendar'
 		},
-		'Contact us': {
+		'lbl_Contact_us': {
 			'vn': 'Liên Hệ',
 			'en': 'Contact us'
 		},
-		'My Cart': {
+		'lbl_My_Cart': {
 			'vn': 'Giỏ Hàng',
 			'en': 'My Cart'
 		},
-		'Account Info': {
+		'lbl_Account_Info': {
 			'vn': 'Thông Tin Tài Khoản',
 			'en': 'Account Info'
 		},
-		'Address Book': {
+		'lbl_Address_Book': {
 			'vn': 'Địa Chỉ Giao Hàng',
 			'en': 'Address Book'
 		},
-		'Orders History': {
+		'lbl_Orders_History': {
 			'vn': 'Lịch Sử Giao Hàng',
 			'en': 'Orders History'
 		},
-		'Log Out': {
+		'lbl_Log_Out': {
 			'vn': 'Đăng Xuất',
 			'en': 'Log Out'
 		},
-		'Sign In': {
+		'lbl_Sign_In': {
 			'vn': 'Đăng Nhập',
 			'en': 'Sign In'
 		},
-		'Register Here': {
+		'lbl_Register_Here': {
 			'vn': 'Đăng Ký',
 			'en': 'Register Here'
 		},
-		'Total': {
+		'lbl_Total': {
 			'vn': 'Tổng',
 			'en': 'Total'
+		},
+		'FEATURED PRODUCTS': {
+			'vn': 'SAN PHAM TIEU BIEU',
+			'en': 'FEATURED PRODUCTS'
 		}
 	}
 
